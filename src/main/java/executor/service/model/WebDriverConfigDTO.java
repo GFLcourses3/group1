5c61fd267b28aca05e5721d21553d511b26a5829
Kostyantyn Dobriohlo <kostyantyn.dobriohlo@avataa.com>
2023-02-14 09:57:05 +0200

    Override equals and hasCode methods

--- src/main/java/executor/service/model/WebDriverConfigDTO.java
+++ src/main/java/executor/service/model/WebDriverConfigDTO.java
@@ -1,12 +1,15 @@
 package executor.service.model;
 
+import java.util.Objects;
+
 public class WebDriverConfigDTO {
     private String webDriverExecutable;
     private String userAgent;
     private Long pageLoadTimeout;
     private Long implicitlyWait;
 
-    public WebDriverConfigDTO() {}
+    public WebDriverConfigDTO() {
+    }
 
     public WebDriverConfigDTO(String webDriverExecutable, String userAgent, Long pageLoadTimeout, Long implicitlyWait) {
         this.webDriverExecutable = webDriverExecutable;
@@ -45,5 +48,19 @@ public class WebDriverConfigDTO {
 
     public void setImplicitlyWait(Long implicitlyWait) {
         this.implicitlyWait = implicitlyWait;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WebDriverConfigDTO that = (WebDriverConfigDTO) o;
+        return webDriverExecutable.equals(that.webDriverExecutable) && userAgent.equals(that.userAgent) &&
+                pageLoadTimeout.equals(that.pageLoadTimeout) && implicitlyWait.equals(that.implicitlyWait);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(webDriverExecutable, userAgent, pageLoadTimeout, implicitlyWait);
     }
 }
