76aa90908419c86dd2e9ee5b1cda37c2b4e31118
Tymur <66021340+TNiurhechev@users.noreply.github.com>
2023-02-20 19:02:43 +0200

    Update ProxyConfigHolderDTO.java
    Commit

--- src/main/java/executor/service/model/ProxyConfigHolderDTO.java
+++ src/main/java/executor/service/model/ProxyConfigHolderDTO.java
@@ -1,4 +1,44 @@
 package executor.service.model;
 
+import java.util.Objects;
+
 public class ProxyConfigHolderDTO {
+    private ProxyNetworkConfigDTO proxyNetworkConfig;
+    private ProxyCredentials proxyCredentials;
+
+    public ProxyConfigHolderDTO() {}
+
+    public ProxyConfigHolderDTO(ProxyNetworkConfigDTO proxyNetworkConfig, ProxyCredentials proxyCredentials) {
+        this.proxyNetworkConfig = proxyNetworkConfig;
+        this.proxyCredentials = proxyCredentials;
+    }
+
+    public ProxyNetworkConfigDTO getProxyNetworkConfig() {
+        return proxyNetworkConfig;
+    }
+
+    public void setProxyNetworkConfig(ProxyNetworkConfigDTO proxyNetworkConfig) {
+        this.proxyNetworkConfig = proxyNetworkConfig;
+    }
+
+    public ProxyCredentials getProxyCredentials() {
+        return proxyCredentials;
+    }
+
+    public void setProxyCredentials(ProxyCredentials proxyCredentials) {
+        this.proxyCredentials = proxyCredentials;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProxyConfigHolderDTO proxyConfigHolder = (ProxyConfigHolderDTO) o;
+        return Objects.equals(proxyNetworkConfig, proxyConfigHolder.proxyNetworkConfig) && Objects.equals(proxyCredentials, proxyConfigHolder.proxyCredentials);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(proxyNetworkConfig, proxyCredentials);
+    }
 }
